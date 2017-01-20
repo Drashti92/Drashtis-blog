2aad11a6e9cbe2dbf424bc45dd67d85088eb8a2e
drashtiblimbachiya <drashtil92@gmail.com>
2017-01-20 01:40:59 +0000

    added partial,destroy button and edited home.html.erb

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -19,24 +19,33 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
+    
+     def edit
+        @article = Article.find(params[:id])
+     end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
 
     def show
         @article = Article.find(params[:id])
     end
     
-    def edit
-        @article = Article.find(params[:id])
-    end
+   
     
-    def update
+     def destroy
         @article = Article.find(params[:id])
-        if @article.update(article_params)
-        flash[:notice] = "Article was successfully updated"
-        redirect_to article_path(@article)
-        else
-        render 'edit'
-        end
-    end
+        @article.destroy
+        flash[:notice] = "Article was successfully deleted"
+        redirect_to articles_path
+     end
     
     private
         def article_params
