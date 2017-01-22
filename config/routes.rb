f9409bc39f1b72beeb154cb8265f5222f8935636
drashtiblimbachiya <drashtil92@gmail.com>
2017-01-22 22:08:51 +0000

    added user,user tables,user info in the article

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  resources :users
+  resources :users
   root 'welcome#home'
   get 'about', to: 'welcome#about'
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119003822) do
+ActiveRecord::Schema.define(version: 20170122090608) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
