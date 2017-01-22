06b424cde34925e9fd260ecda9e0e69a991237d5
drashtiblimbachiya <drashtil92@gmail.com>
2017-01-22 22:19:10 +0000

    added encrypted password

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'social-share-button', '~> 0.1.10'
 
+gem 'bcrypt', '~> 3.1.7'
+
 
 
 # Use ActiveModel has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122090608) do
+ActiveRecord::Schema.define(version: 20170122220951) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20170122090608) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
