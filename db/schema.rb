40026a06ac41c8839b432f4eb164505f8829962b
drashtiblimbachiya <drashtil92@gmail.com>
2017-01-24 09:45:53 +0000

    modified articles stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122220951) do
+ActiveRecord::Schema.define(version: 20170124090334) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20170122220951) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
