f0c400acc4fc956faed5332fbbd65c120dbb3464
drashtiblimbachiya <drashtil92@gmail.com>
2017-01-25 08:29:21 +0000

    added categories to UI

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
+  resources :categories, except: [:destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
